4925d2291521ef2658038eea606430b29b866a12
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-03-11 14:30:22 -0300

    v6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         <>
             <div className={styles.headerContent}>
                 <NavLink to="/" className={styles.brand}>
-                    <img src="../../public/logoif" alt="ifmobile" />
+                    <img src="../../public/logoif.svg" alt="ifmobile" />
                 </NavLink>
             </div>
             <nav className={styles.navbar}>
